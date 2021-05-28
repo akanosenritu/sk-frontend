d9a3090ff05b94d8366bf3a806fdebc3ae9baa1a
benjaponum <benjaponum@gmail.com>
2021-05-28 21:47:18 +0900

    add handler for network errors

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -26,93 +26,134 @@ export const getCsrfToken = async (): Promise<string> => {
 }
 
 export const get = async <T>(target: string): Promise<SuccessWithData<T>|Failure> => {
-  const response = await fetch(apiURL + target)
-  if (response.ok) {
-    return {
-      ok: true,
-      data: await response.json() as T
+  try {
+    const response = await fetch(apiURL + target)
+    if (response.ok) {
+      return {
+        ok: true,
+        data: await response.json() as T
+      }
     }
-  }
-  return {
-    ok: false,
-    description: response.statusText
+    return {
+      ok: false,
+      description: response.statusText
+    }
+  } catch (e) {
+    return {
+      ok: false,
+      description: "unknown error",
+      data: e
+    }
   }
 }
 
 export const post = async <T>(target: string, data: T): Promise<SuccessWithData<T>|Failure> => {
-  const csrfToken = await getCsrfToken()
-  const response = await fetch(apiURL + target, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": csrfToken
-    },
-    body: JSON.stringify(data)
-  })
-  if (response.ok) {
-    const returnedData = await response.json()
-    return {
-      ok: true,
-      data: returnedData as T
+  try {
+    const csrfToken = await getCsrfToken()
+    const response = await fetch(apiURL + target, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrfToken
+      },
+      body: JSON.stringify(data)
+    })
+    if (response.ok) {
+      const returnedData = await response.json()
+      return {
+        ok: true,
+        data: returnedData as T
+      }
     }
-  }
-  return {
-    ok: false,
-    description: response.statusText,
-    data: await response.json(),
+    return {
+      ok: false,
+      description: response.statusText,
+      data: await response.json(),
+    }
+  } catch (e) {
+    return {
+      ok: false,
+      description: "unknown error",
+      data: e
+    }
   }
 }
 
 export const postWritable = async <T, T2>(target: string, data:T): Promise<SuccessWithData<T2>|Failure> => {
-  const result = await post<T>(target, data)
-  console.log(result)
-  if (result.ok) {
+  try {
+    const result = await post<T>(target, data)
+    if (result.ok) {
+      return {
+        ...result,
+        data: result.data as unknown as T2
+      }
+    }
+    if (result.data && result.data.uuid) {
+      const uuidErrorMessage = (result.data.uuid as string[])[0]
+      if (uuidErrorMessage.match(/with this uuid already exists\.$/)) {
+        // @ts-ignore
+        return putWritable<T, T2>(target + data.uuid + "/", data)
+      }
+    }
+    return result
+  } catch (e) {
     return {
-      ...result,
-      data: result.data as unknown as T2
+      ok: false,
+      description: "unknown error",
+      data: e
     }
   }
-  if (result.data && result.data.uuid) {
-    const uuidErrorMessage = (result.data.uuid as string[])[0]
-    if (uuidErrorMessage.match(/with this uuid already exists\.$/)) {
-      // @ts-ignore
-      return putWritable<T, T2>(target + data.uuid + "/", data)
-    }
-  }
-  return result
+
 }
 
 export const put = async <T>(target: string, data: T): Promise<SuccessWithData<T>|Failure> => {
-  const csrfToken = await getCsrfToken()
-  const response = await fetch(apiURL + target, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-      "X-CSRFToken": csrfToken
-    },
-    body: JSON.stringify(data)
-  })
-  if (response.ok) {
-    const returnedData = await response.json()
+  try {
+    const csrfToken = await getCsrfToken()
+    const response = await fetch(apiURL + target, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrfToken
+      },
+      body: JSON.stringify(data)
+    })
+    if (response.ok) {
+      const returnedData = await response.json()
+      return {
+        ok: true,
+        data: returnedData as T
+      }
+    }
     return {
-      ok: true,
-      data: returnedData as T
+      ok: false,
+      description: response.statusText,
+      data: await response.json(),
+    }
+  } catch (e) {
+    return {
+      ok: false,
+      description: "unknown error",
+      data: e
     }
   }
-  return {
-    ok: false,
-    description: response.statusText,
-    data: await response.json(),
-  }
+
 }
 
 export const putWritable = async <T, T2>(target: string, data:T): Promise<SuccessWithData<T2>|Failure> => {
-  const result = await put<T>(target, data)
-  if (result.ok) {
+  try {
+    const result = await put<T>(target, data)
+    if (result.ok) {
+      return {
+        ...result,
+        data: result.data as unknown as T2
+      }
+    }
+    return result
+  } catch (e) {
     return {
-      ...result,
-      data: result.data as unknown as T2
+      ok: false,
+      description: "unknown error",
+      data: e
     }
   }
-  return result
 }
